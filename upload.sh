790173e472b9ff48d9ffa12199ebe8570638a5b8
Danny1127 <zhangwendan@qq.com>
2020-10-21 19:47:40 -0700

    updated website links

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
 
-#scp -r $@ wzhang357@u.arizona.edu:public_html/.
+scp -r $@ wzhang357@u.arizona.edu:public_html/.
 #scp $@ wzhang357@u.arizona.edu:public_html/parts/.
 
-scp -r $1 wzhang357@u.arizona.edu:public_html/$2
