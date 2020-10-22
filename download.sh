4cf482c4b02faab02a033155b42b349c861b2874
Danny1127 <zhangwendan@qq.com>
2020-10-21 19:32:41 -0700

    Update link to department

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 
-scp -r wzhang357@u.arizona.edu:public_html/parts/* parts/.
+#scp -r wzhang357@u.arizona.edu:public_html/parts/* parts/.
+scp -r wzhang357@u.arizona.edu:public_html/* .
+
 #scp wzhang357@u.arizona.edu:public_html/*.html .
 
 #scp wzhang357@u.arizona.edu:public_html/paper/* paper/.
--- download_website.sh
+++
@@ -1,3 +0,0 @@
-
-scp -r wzhang357@u.arizona.edu:public_html/* .
-
